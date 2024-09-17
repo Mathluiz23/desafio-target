c132326dcf87b23fda74db2ebbe7527e45fd3101
Mathluiz23 <matheus_luizs@hotmail.com>
2024-09-17 20:15:03 -0300

    :lipstick: ajustando indentação do desafio 1 com prettier

--- desafios/desafio1.js
+++ desafios/desafio1.js
@@ -1,18 +1,17 @@
 const index = 13;
 
 function calculateSum(n) {
-  let sum = 0;
-  let k = 0;
+	let sum = 0;
+	let k = 0;
 
-  while (k < n) {
-    k += 1;
-    sum += k;
-  }
+	while (k < n) {
+		k += 1;
+		sum += k;
+	}
 
-  return sum;
+	return sum;
 }
 
 const result = calculateSum(index);
 
 console.log(`O valor da variável SOMA é ${result}.`);
-
