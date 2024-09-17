b7adf05f8d73db9d3fa3dc435566e91424b718b3
Mathluiz23 <matheus_luizs@hotmail.com>
2024-09-17 20:10:18 -0300

    :sparkles: feat: Implementando desafio 1 - adiciona função para calcular a soma dos inteiros até um índice dado

---
+++ desafios/desafio1.js
@@ -0,0 +1,18 @@
+const index = 13;
+
+function calculateSum(n) {
+  let sum = 0;
+  let k = 0;
+
+  while (k < n) {
+    k += 1;
+    sum += k;
+  }
+
+  return sum;
+}
+
+const result = calculateSum(index);
+
+console.log(`O valor da variável SOMA é ${result}.`);
+
